a4e5d08a062f247aa683c0471c8c8d86b2e8808c
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-15 16:49:24 -0700

    4-c_task commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main(void) 
-{ 
-puts("\"Programming is like building a multilingual puzzle");
-return (0); 
+
+/**
+ * print word Programming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle\n");
+  return (0);
 }
