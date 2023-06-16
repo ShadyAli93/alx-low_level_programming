57765d96c2081f51ca39421609155ca4422092e6
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-16 13:16:57 -0700

    4-c-task commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - for loop with if
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
