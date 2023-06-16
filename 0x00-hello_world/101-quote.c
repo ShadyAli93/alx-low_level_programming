844d4192fa753e96dbb3bd497dfa744a52de6041
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-15 18:25:19 -0700

    101-c_task commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - script to print word 
+ * main - script to print word
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",61);
 return (1);
 }
 
