5a96d6b6f6783c28a79c97ea96e2f514c5417747
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-16 13:02:02 -0700

    3-c-task commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - for loop
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char l;
+chat u;
+for (l = 'a'; l <= 'z'; l++)
+{
+putchar(l);
+}
+for (u = 'A'; u <= 'Z'; u++)
+{
+putchar(u)
+}
+putchar('\n');
+return (0);
+}
