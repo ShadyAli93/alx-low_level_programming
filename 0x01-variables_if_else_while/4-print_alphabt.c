f0ab4158f0ce58ebcf0a55595780fb2dcc3aade1
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-16 13:12:46 -0700

    4-c-task commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - for loop with if
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char l;
+for (l = 'a'; l <= 'z'; l++)
+if (b != 'q' && b != 'e')
+putchar(l)
+putchar('\n');
+return (0);
+}
