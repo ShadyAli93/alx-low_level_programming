c4bf577ad8ca535fcf999b967325400ad86db598
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-16 14:12:35 -0700

    7-c-task commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - for loop with if
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char l;
+for (l = 'z'; l > 'a'; l--)
+putchar(l);
+putchar('\n');
+return (0);
+}
