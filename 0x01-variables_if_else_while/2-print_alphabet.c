655c064f1f5ddec8f23ae22864809805e929d682
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-16 12:28:03 -0700

    2-c-task commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - for loop
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+for (int x = 'A'; x <= 'Z'; x++)
+{
+putchar(tolower(x));
+}
+return (0);
+}
