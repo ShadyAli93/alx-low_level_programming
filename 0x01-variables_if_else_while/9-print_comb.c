ba97649bb27d75e6e508e4a86d36f0032dbe66a6
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-16 14:33:08 -0700

    9-c-task commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - for loop with if
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 0;
+for (n = 0; n <= 9; n++)
+{
+putchar('0' + n);
+if (n <= 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
