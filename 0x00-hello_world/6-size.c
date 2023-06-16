285ed478006f15dae6c1cd0a46ae1a31dbb3a97f
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-15 17:20:59 -0700

    6-c_task commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  program that prints the size of various types on the computer
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char a;
+int b;
+long c;
+long long d;
+float f;
+printf("Size of a char:%d byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int:%d byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int:%d byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int:%d byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float:%d byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
