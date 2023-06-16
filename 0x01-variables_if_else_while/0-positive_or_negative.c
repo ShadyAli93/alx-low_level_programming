aa375abcd31f53957076eb6602f91e5b71f6b27b
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-16 10:46:23 -0700

    0-c-task commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,14 +12,14 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf(n, "is postive");
+printf(n, "%d is postive\n");
 }
 else if (n == 0)
 {
-printf(n, "is zero");
+printf(n, "%d is zero\n");
 }
 else
 {
-printf(n, "is negative");
+printf(n, "%d is negative\n");
 return (0);
 }
