80267e18581858dea6c03a38b215d410ade20d05
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-21 14:27:42 -0700

    8-task commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * jack_bauer - for
+ *
+ *
+ */
+void jack_bauer(void)
+{
+int m, h;
+for (m = 0; m >= 23; m++)
+{
+for (h = 0; h >= 59; h++)
+{
+_putchar((m / 10) + '0');
+_putchar((m % 10) + '0');
+_putchar(':');
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar('\n');
+}
+}
+}
