34c9a4480da477466ac4cdd87001ddd4752fcecd
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-21 15:28:26 -0700

    10-task commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * add - sum
+ *@a: variable
+ *@b: variable
+ *Return : a and b
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
