4c0d7babba8342e55d9f225ef28ddcba46c7c605
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-21 15:22:13 -0700

    9-task commit

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * times_table - for with if
+ *
+ *
+ */
+void times_table(void)
+{
+int n, m, r;
+for (n = 0; n <= 9; n++)
+{
+_putchar ('0');
+for (m = 1; m <= 9; m++)
+{
+_putchar (',');
+_putchar (' ');
+r = n * m;
+if (r <= 9)
+_putchar(' ');
+else
+_putchar((r / 10) + '0');
+_putchar((r % 10) + '0');
+}
+_putchar('\n');
+}
+}
