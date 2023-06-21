153f6f0b37ba1a07e327806aa5929651823ccf29
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-21 06:16:36 -0700

    6-task commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _abs - if
+ *@i: variable
+ *Return: i
+ */
+int _abs(int i)
+{
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (-i);
+}
+}
