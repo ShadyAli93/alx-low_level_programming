309a36c7575a5ac33872766a6cd5364d469da9ae
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-22 15:05:53 -0700

    6-task commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * print_line - for loop
+ *
+ *
+ *
+ */
+void print_line(int n)
+{
+int l;
+if l <= 0
+{
+_putchar('\n');
+}
+else
+{
+for (l = 0; l <= n; l++)
+{
+_putchar('_');
+}
+_puthcat('\n');
+}
+}
