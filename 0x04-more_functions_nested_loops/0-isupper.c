061e390aa8f379023c216d87fbb0c5542178d88b
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-22 06:37:51 -0700

    0-task commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _isupper - if
+ *@c: variable
+ *Return: 0 and 1
+ */
+int _isupper(int c)
+{
+if (n >= 'A' && n >= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
