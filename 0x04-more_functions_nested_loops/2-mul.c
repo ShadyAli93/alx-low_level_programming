125d2f76784bcd22c1d29890d2bfe7d4ee47c6bf
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-22 08:39:32 -0700

    2-task commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * mul - multi
+ *@a: variable
+ *@b: variable
+ *Return: 0 and 1
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
