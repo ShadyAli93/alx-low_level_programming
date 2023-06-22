106e453b8afb3e717c17c6dc28f9cc9b760d45ec
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-22 14:34:07 -0700

    5-task commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * more_numbers - for loop
+ *
+ *
+ *
+ */
+void more_numbers(void)
+{
+int n, l;
+for (l = 0; l <= 10; l++)
+{
+for (n = '0'; n <= 14; n++)
+_putchar(n);
+}
+_putchar('\n');
+}
