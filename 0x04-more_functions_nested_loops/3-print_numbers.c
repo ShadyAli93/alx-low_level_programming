4d6f978bb868927e18fb475e07f4d86d4ce5de14
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-22 09:17:19 -0700

    3-task commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * print_numbers - for
+ *
+ *
+ *
+ */
+void print_numbers(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{	
+_putchar(n);
+}
+_putchar('\n');
+}
