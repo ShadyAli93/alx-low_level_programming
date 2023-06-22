c78aa0e015792b309683f571e49cbdb621280fcd
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-22 09:51:29 -0700

    4-task commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,8 +11,8 @@
 void print_most_numbers(void)
 {
 int n;
-for (n = 0; n <= 9; n++)
-if ((n != 2) && (n != 4))
+for (n = '0'; n <= '9'; n++)
+if ((n != '2') && (n != '4'))
 _putchar(n);
 _putchar('\n');
 }
