98b8abcf7ef58a496e8803820f01fb15c4d2af23
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-22 14:51:57 -0700

    5-task commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,16 +11,16 @@
 void more_numbers(void)
 {
 int n, l;
-for (l = 0; l <= 10; l++)
+for (l = 0; l < 10; l++)
 {
 for (n = 0; n <= 14; n++)
 {
 if (n > 9)
-{	
+{
 _putchar((n / 10) + '0');
 }
 _putchar((n % 10) + '0');
 }
-}
 _putchar('\n');
+}
 }
