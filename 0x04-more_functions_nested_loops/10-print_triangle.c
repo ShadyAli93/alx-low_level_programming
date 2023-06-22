3b63d4ff78de3dfbf2377647ffb57cabff77b6e8
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-22 16:51:11 -0700

    10-task commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * print_triangle - for loop
+ *@size: variable
+ *
+ *
+ */
+void print_triangle(int size)
+{
+int r, c;
+if (size <= 0)
+{
+_putchar('\n');
+}
+for (r = 1; r <= size; r++)
+{
+for (c = 1; c <= r; c++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
