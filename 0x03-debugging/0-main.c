0c30adaf95e03de2ff7b9277f2ab1b0bd74e2755
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-23 10:24:26 -0700

    0-task commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,28 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
+#include "main.h"
 /**
- * main - if condition
- *
- * Return: Always 0 (Success)
- */
+* main - tests
+*
+* Return: 0
+*/
+
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-{
-printf("%d is positive\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else
-{
-printf("%d is negative\n", n);
-}
+int i;
+i = 0;
+positive_or_negative(i);
 return (0);
 }
