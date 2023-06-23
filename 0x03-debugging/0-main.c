cce3eca76f39ba74d0039fb32c4c2d1e51f6652a
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-23 12:01:38 -0700

    0-task commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -10,7 +8,9 @@
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
