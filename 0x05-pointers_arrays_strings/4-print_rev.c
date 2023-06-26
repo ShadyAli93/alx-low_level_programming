4b020ffbfffd57f58d2e74f34845eec41e2e333a
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-26 16:00:25 -0700

    4-task commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_rev - print pointer
+*@s: variables
+*
+*
+*/
+
+void print_rev(char *s)
+{
+int c, r;
+for (c = 0; s[c] != '\0'; c++)
+{
+for (r = c; r >= 0; r--)
+{
+_putchar(s[r]);
+}
+}
+_putchar('\n');
+}
