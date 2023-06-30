fce938736b2a917445586a1bbcc503ad448785b0
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-06-29 17:12:54 -0700

    1-task commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two strings
+ * @dest: variables
+ * @src: variables
+ * @n: variables
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int d;
+
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+d = 0;
+while (d < n && src[d] != '\0')
+{
+dest[i] = src[d];
+i++;
+d++;
+}
+dest[i] = '\0';
+return (dest);
+}
+
