3cc5d03bd9606979b9e288bcdaa944f75d30801e
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-03 08:36:33 -0700

    2-task commit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,23 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * _strchr -  locates a character
  * @s: variable
  * @c: variable
  * Return: s
  */
-char *_strchr(char *s, char c) 
+char *_strchr(char *s, char c)
 {
-for (; *s != '\0'; s++) 
+for (; *s != '\0'; s++)
 {
-if (*s == c) 
-{
-return (s);
-}
-}
-if (c == '\0') 
+if (*s == c)
 {
 return (s);
 }
-else
+}
+if (c == '\0')
 {
+return (s);
+}
 return (NULL);
 }
