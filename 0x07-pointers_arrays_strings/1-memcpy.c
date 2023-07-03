5606411beda1a862d3777e97a6d4117f29c40e45
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-03 10:00:26 -0700

    2-task commit

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,14 +8,8 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-char *pd = dest;
-char *ps = src;
-while (n > 0)
-{
-*pd = *ps;
-pd++;
-ps++;
-n--;
-}
+int i = 0;
+for (; i < n; i++)
+dest[i] = src[i];
 return (dest);
 }
