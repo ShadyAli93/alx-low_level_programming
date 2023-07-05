7ba0487f9b5637b2a083502e68d4f0c5c7f45260
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-05 15:30:26 -0700

    3-task commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial -  fac
+ * @n: variable
+ *Return: 1 and -1
+ */
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (1);
+}
+else
+{
+return (n * factorial(n - 1));
+}
+}
