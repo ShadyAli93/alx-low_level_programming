41c2a6659a0e2d63db5acb33641682a8392f36e5
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-05 15:37:07 -0700

    4-task commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _pow_recursion -  pow
+ * @x: variable
+ *@y: variable
+ *Return: 1 and -1
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0) 
+{
+return (-1);
+}
+else if (y == 0) 
+{
+return (1);
+}
+else 
+{
+return (x * _pow_recursion(x, y - 1));
+}
+}
