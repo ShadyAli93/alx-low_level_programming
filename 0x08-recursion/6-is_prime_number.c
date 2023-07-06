e59b8e45f0b9e9e44b50177d6a8b628dcb4b600f
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-06 15:58:19 -0700

    6-task commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,7 @@ int prime_number(int x, int y)
 		return (1);
 	else if (x % y == 0 && x > 0)
 		return (0);
-	return (prime_number(x, y + 1));
+	return (prime_number(x, y - 1));
 }
 /**
  * is_prime_number - primenumber
