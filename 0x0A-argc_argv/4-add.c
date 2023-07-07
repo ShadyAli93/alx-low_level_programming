adf901136b91fb7c2360bb2418ba672c880ac717
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-07 09:44:28 -0700

    4-task commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,13 +15,15 @@ if (argc < 2)
 {
 printf("%d\n", 0);
 }
-else if ((argc >= 'a' && argc >= 'z') || (argc >= 'A' && argc >= 'Z'))
+else
+for (i = 0; i < argc; i++)
+{
+if ((argc >= 'a' && argc >= 'z') || (argc >= 'A' && argc >= 'Z'))
 {
 printf("%s\n", "Error");
 return (1);
 }
-else
-for (i = 0; i < argc; i++)
+}
 sum += atoi(argv[i]);
 printf("%d\n", sum);
 return (0);
