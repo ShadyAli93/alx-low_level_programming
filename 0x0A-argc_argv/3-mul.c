9feebdf84b8d71abb37b3f96672a2dcd28a21706
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-07 07:32:13 -0700

    2-task commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - argc and argv
+* @argc: variable
+* @argv: variable
+* Return: 0;
+*/
+int main(int argc, char *argv[])
+{
+int i, mlti = 0;
+for (int i; i < argc; i++)
+printf("s\n", argv[i]);
+multi = multi * argv[i];
+return (0);
+}
