bd6cbaa74b0304d5bb3b46d1010002ffab0682de
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-07 06:06:05 -0700

    0-task commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - argc and argv
+* @argc: variable
+* @argv: variable
+* Return: 0;
+*/
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
