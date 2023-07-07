a767cf0b3100cf53063ad5749d6e1dab42f0f121
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-07 09:13:09 -0700

    4-task commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+* main - argc and argv
+* @argc: variable
+* @argv: variable
+* Return: 0 and 1;
+*/
+int main(int argc, char *argv[])
+{
+int i, sum = 0;
+if (argc < 2)
+printf("%d\n", 0);
+else
+for (i = 0; i < argc; i++)
+sum += atoi(argv[i]);
+printf("%d\n", sum);
+return (0);
+}
