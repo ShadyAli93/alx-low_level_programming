4a4ec9d8df1d5297987552c552a684bedaa3e7da
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-11 16:39:04 -0700

    4-task commit

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,19 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
 * free_grid - free
 * @grid: variable
 * @height: variable
-* Return: 0;
+* Return: 0
 */
 void free_grid(int **grid, int height)
 {
 int i;
-for (i = 0; i < height; i++)		
+for (i = 0; i < height; i++)
 {
-free(grid[i]);		
-}	
-free(grid);		
+free(grid[i]);
+}
+free(grid);
 }
