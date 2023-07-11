cd6334eef3b4965fce55240a4124d9746d54b4ad
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-11 15:49:54 -0700

    4-task commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* free_grid - free
+* @grid: variable
+* @height: variable
+* Return: 0;
+*/
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)		
+{
+free(grid[i]);		
+}	
+free(grid);		
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+void free_grid(int **grid, int height);
