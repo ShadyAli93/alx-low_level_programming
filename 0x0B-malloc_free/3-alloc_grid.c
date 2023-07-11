1dd3d4d2045e9b85d10deb0c530ad690aae051c3
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-11 16:33:05 -0700

    3-task commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - grid
+ * @width: variable
+ * @height: variable
+ * Return: null and pntr
+ */
+int **alloc_grid(int width, int height)
+{
+int **pntr;
+int i, n;
+if (width < 1 || height < 1)
+return (NULL);
+pntr = malloc(height * sizeof(int *));
+if (gridout == NULL)
+{
+free(pntr);
+return (NULL);
+}
+for (i = 0; i < height; i++)
+{
+pntr[i] = malloc(width * sizeof(int));
+if (pntr[i] == NULL)
+{
+for (i--; i >= 0; i--)
+free(pntr[i]);
+free(pntr);
+return (NULL);
+}
+}
+for (i = 0; i < height; i++)
+for (n = 0; n < width; n++)
+gridout[i][n] = 0;
+return (pntr);
+}
+
