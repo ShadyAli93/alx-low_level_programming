fe27d2a0e9334c2090a914534caa41974ddbfad3
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-11 04:50:37 -0700

    0-task commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,11 +10,9 @@ char *create_array(unsigned int size, char c)
 {
 char *pntr;
 unsigned int i;
-if (size == 0)
-return (NULL);
 pntr = malloc(sizeof(c) * size);
-if (pntr == NULL)
-return (NULL);
+if (size == '0' || pntr == NULL)
+return ("NULL");
 for (i = 0; i < size; size++)
 pntr[i] = c;
 return (pntr);
