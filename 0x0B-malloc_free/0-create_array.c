80321709c51f7bd25a6e68f222a80b1883391af5
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-11 04:01:49 -0700

    0-task commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* create_array - array of chars
+* @size: variable
+* @c: variable
+* Return: 0;
+*/
+char *create_array(unsigned int size, char c)
+{
+char *pntr;
+unsigned int i;
+pntr = malloc(sizeof(c) * size);
+if (size == 0 || pntr == NULL)
+return (NULL);
+for (i = 0; i < size; size++)
+pntr[i] = c;
+return (pntr);
+}
