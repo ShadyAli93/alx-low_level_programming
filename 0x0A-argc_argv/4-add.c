6e7e381ed1e71425c17415ae67aeb089ec31ad76
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-12 14:17:36 -0700

    4-task commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,12 +11,13 @@
 */
 int main(int argc, char *argv[])
 {
-int i, sum = 0;
+int i, c, sum = 0;
 if (argc == 0)
 return (0);
 for (i = 1; i < argc; i++)
 {
-if (!isdigit(*argv[i]))
+for (c = 0; argv[i][c] != '\0'; c++)
+if (!isdigit(argv[i][c]))
 {
 printf("%s\n", "Error");
 return (1);
