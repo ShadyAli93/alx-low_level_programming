b2267f6bc6d283024780d1c3d8ecd74259cdc8ae
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-12 14:11:40 -0700

    100-task commit

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - cent
+ * @argc: variable
+ * @argv: variable
+ * Return: null and pntr
+ */
+int main(int argc, char *argv[])
+{
+int  cent, coins;
+if (argc != 2)
+{
+printf("%s\n", "Error");
+return (1);
+}
+cent = atoi(argv[1]);
+while (cent >= 25)
+{
+cent -= 25;
+coins++;
+}
+while (cent >= 10)
+{
+cent -= 10;
+coins++;
+}
+while (cent >= 5)
+{
+cent -= 5;
+coins++;
+}
+while (cent >= 2)
+{
+cent -= 2;
+coins++;
+}
+if (cent == 1)
+coins++;
+printf("%d\n", coins);
+return (0);
+}
