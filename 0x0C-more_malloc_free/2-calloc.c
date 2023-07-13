0395d3d24ac878d23ac6753df7d03985ec343c15
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-13 14:27:52 -0700

    2-task commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,10 +10,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *pntr;
 unsigned int i;
-if (nmemb == 0 || size == 0)
-return (NULL);
 pntr = malloc(nmemb * size);
-if (pntr == NULL)
+if ((nmemb == 0 || size == 0) && (pntr == NULL))
 return (NULL);
 for (i = 0; i < (nmemb * size); i++)
 pntr[i] = 0;
