1295244183c39e698b084dccb298a76b9ecdd4fd
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-17 15:21:42 -0700

    0-task commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - newdog
+ * @name: variable
+ * @age: variable
+ * @owner: variable
+ * Return: pntr
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *pntr;
+pntr = malloc(sizeof(dog_t));
+if (p == NULL)
+return (NULL);
+if (name == NULL)
+{
+free(pntr);
+free(owner);
+return (NULL);
+}
+if (owner == NULL)
+{
+free(pntr);
+free(name);
+return (NULL);
+}
+pntr->name = name;
+pntr->age = age;
+pntr->owner = owner;
+return (pntr);
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - dog
+ * @d: variable
+ * Return: null
+ */
+void free_dog(dog_t *d)
+{
+if (d == 0)
+return;
+free(d->name);
+free(d->age);
+free(d->owner);
+}
