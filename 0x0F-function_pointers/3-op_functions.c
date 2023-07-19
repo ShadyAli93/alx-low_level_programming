f98163ecad0d8c9f6ad8e86cfb27aea13f0488d1
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-19 14:15:41 -0700

    3-task commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - cmp
+ * @a: variable
+ * @b: variable
+ * Return: result
+ */
+int op_add(int a, int b);
+{
+return (a + b);
+}
+/**
+ * op_sub - sub
+ * @a: variable
+ * @b: variable
+ * Return: result
+ */
+int op_sub(int a, int b);
+{
+return (a - b);
+}
+/**
+ * op_mul - mul
+ * @a: variable
+ * @b: variable
+ * Return: result
+ */
+int op_mul(int a, int b);
+{
+return (a * b);
+}
+/**
+ * op_div - div
+ * @a: variable
+ * @b: variable
+ * Return: result
+ */
+int op_div(int a, int b);
+{
+return (a / b);
+}
+/**
+ * op_mod - mod
+ * @a: variable
+ * @b: variable
+ * Return: result
+ */
+int op_mod(int a, int b);
+{
+return (a % b);
+}
