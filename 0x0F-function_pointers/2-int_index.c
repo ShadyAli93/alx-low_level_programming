f59b6b42c5e2442d87737912701b33740fcffe3f
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-19 13:46:01 -0700

    2-task commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - cmp
+ * @array: variable
+ * @size: variable
+ * @cmp: variable
+ * Return: -1 and i
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (array == NULL || cmp == NULL)
+{
+return (-1);
+}
+if (size <= 0)
+{
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+cmp(array[i]);
+}
+return (i);
+}
