cfa1df6c934e93290012579e52e3cd5319623be5
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-20 15:20:02 -0700

    1-task commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,10 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list ap;
 unsigned int i;
 va_start(ap, n);
-for (i = 0; i < count; i++)
-printf("%d", va_arg(ap, int);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(ap, int));
 if (separator != NULL)
+{
 printf("%s", separator);
+}
+}
 va_end(ap);
 printf("\n");
 }
