08c29997f545ea75da71d808d66b390c3ffea609
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-07-20 16:28:29 -0700

    3-task commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,40 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_all - print
+ * @format: variable
+ * Return: 0
+ */
+void print_all(const char * const format, ...)
+{
+va_list ap;
+int i = 0;
+char *s;
+va_start(ap, format);
+while (format != NULL)
+switch (format[i])
+{
+case 'c':
+printf("%c", (char) va_arg(ap, int));
+break;
+case 'i':
+printf("%i", va_arg(ap, int));
+break;
+case 'f':
+printf("%f", va_arg(ap, double));
+break;
+case 's':
+s = va_arg(ap, char *);
+if (s == NULL)
+{
+printf("(nil)");
+break;
+}
+printf("%s", s);
+break;
+}
+va_end(ap);
+printf("\n");
+}
