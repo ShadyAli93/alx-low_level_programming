8d21e9c84387244047f1bb2f7b2880eb7aac6021
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-08-03 16:19:35 -0700

    0-task commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,22 +3,25 @@
 #include <stdlib.h>
 
 /**
- * binary_to_uint - binary_to_uint 
+ * binary_to_uint - binary_to_uint
  *@b: variable
- *Return: add
+ *Return: conv and 0
 */
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int conv;
 if (b == NULL)
 {
-return(0);
+return (0);
 }
+while (b != NULL)
+{
 if (*b < '0' || *b > '1')
 {
 return (0);
 }
 conv <<= 1;
 conv += *b++ - '0';
+}
 return (conv);
 }
