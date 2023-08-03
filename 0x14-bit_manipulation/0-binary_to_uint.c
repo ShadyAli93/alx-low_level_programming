50a989aaa8fa008f5246eb48d307fc0d1b3d4b48
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-08-03 16:28:04 -0700

    0-task commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,18 +10,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int conv = 0;
+int index = 0;
 if (b == NULL)
 {
 return (0);
 }
 while (b != NULL)
 {
-if (*b < 0 || *b > 1)
+if (*b < '0' || *b > '1')
 {
 return (0);
 }
-conv <<= 1;
-conv += *b++ - 0;
+conv = (conv << 1) + (b[index] - '0');
 }
 return (conv);
 }
