e076b1c955075a14771f78704c40518e3bed5475
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-08-03 17:57:15 -0700

    4-task commit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * clear_bit - clear
+ *@n: variable
+ *@index: variable
+ *Return: -1 and 1
+*/
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= (sizeof(unsigned long int) * 8))
+{
+return (-1);
+}
+*n &= ~(1 << index);
+return (1);
+}
