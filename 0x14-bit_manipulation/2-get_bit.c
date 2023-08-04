dc64d35483aece60bcec977db5c5667ecacd34e5
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-08-03 17:36:05 -0700

    2-task commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,7 +12,7 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned long int bit;
-if (index >= (sizeof(unsigned long int)))
+if (index >= (sizeof(unsigned long int) * 8))
 return (-1);
 bit = n >> index;
 return (bit & 1);
