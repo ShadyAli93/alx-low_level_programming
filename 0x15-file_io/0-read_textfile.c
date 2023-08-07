91879f3874b878313918572a09576c2c43df5f87
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-08-07 13:31:51 -0700

    0-task commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - read
+ *@filename: variable
+ *@letters: variable
+ *Return: 0 and 1
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int fd;
+char *buffer;
+ssize_t r;
+ssize_t w;
+if (filename == NULL)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
+r = read(fd, buffer, letters);
+w = write(STDOUT_FILENO, buffer, r);
+free(buffer);
+close(fd);
+return (w);
+}
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,11 @@
 #ifndef MAIN
 #define MAIN
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
