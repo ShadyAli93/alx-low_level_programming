d80039f33a56d892621f3353832a3d273eef4b5c
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-08-07 14:17:25 -0700

    1-task commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 int fd;
-int c = 0;
+int c;
 if (filename == NULL)
 return (-1);
 if (text_content == NULL)
@@ -17,10 +17,7 @@ text_content = "";
 fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
-while (text_content[c] != 0)
-{
-c++;
-}
+for (c = 0; text_content[c] != 0; c++)
 write(fd, text_content, c);
 close(fd);
 return (1);
