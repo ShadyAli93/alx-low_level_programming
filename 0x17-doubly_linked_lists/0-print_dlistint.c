5a1f06d8cdc3fab3e4fb72f0084831f3e19828cd
ShadyAli93 <shady.ibrahim.aly@gmail.com>
2023-09-24 12:27:50 -0700

    0-task commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h:variable
+ * Return: n
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t node = 0;
+while (h)
+{
+node++;
+printf("%d\n", h->n);
+h = h->next;
+}
+return (node);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+task 17 in c
